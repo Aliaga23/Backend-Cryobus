c9a3692c7243a49b94860d573a79d97f2fada2b6
cryobus <jaav2801@gmail.com>
2024-05-29 03:59:35 -0400

    Prueba 2 bit

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -2,6 +2,7 @@ const { getUserById } = require('../models/userModel');
 const bcrypt = require('bcrypt');
 const { addRegistro } = require('../models/bitacoraModel');
 const io = require('../index');
+const fetch = require('node-fetch'); // Asegúrate de que `fetch` esté disponible
 
 const loginUser = async (req, res) => {
   const { id, pass } = req.body;
@@ -14,7 +15,29 @@ const loginUser = async (req, res) => {
     if (!isMatch) {
       return res.status(401).json({ message: 'Contraseña incorrecta' });
     }
-       
+
+    // Obtener IP del cliente
+    const ipAPI = "https://api.ipify.org?format=json";
+    const response = await fetch(ipAPI);
+    const data = await response.json();
+    const ipAddress = data.ip;
+
+    // Registrar la acción en la bitácora
+    const fecha = new Date().toISOString().split('T')[0];
+    const hora = new Date().toTimeString().split(' ')[0];
+    const registro = {
+      IDACCION: 1, // ID de INICIAR SESION
+      IDUSUARIO: user.ID,
+      IP: ipAddress,
+      FECHA: fecha,
+      HORAACCION: hora,
+      ELEMENTOMODIFICADO: 'LOGIN'
+    };
+    const registroId = await addRegistro(registro);
+
+    // Emitir evento de nueva acción
+    io.emit('nuevaAccion', { ...registro, NRO: registroId });
+
     res.json({ message: 'Login exitoso', user: { id: user.ID, apellidos: user.APELLIDOS, nombres: user.NOMBRES }, role: user.IDROL });
   } catch (error) {
     res.status(500).json({ message: 'Error al iniciar sesión', error });
--- models/bitacoraModel.js
+++ models/bitacoraModel.js
@@ -2,7 +2,7 @@ const { pool } = require('../db');
 
 const getAllRegistros = async () => {
   try {
-    const [rows] = await pool.query('SELECT * FROM REGISTROACCION');
+    const [rows] = await pool.query('SELECT * FROM REGISTROACCION ORDER BY NRO DESC');
     return rows;
   } catch (error) {
     throw new Error(error.message);
@@ -10,10 +10,12 @@ const getAllRegistros = async () => {
 };
 
 const addRegistro = async (registro) => {
-  const { IDACCION, IDUSUARIO, IP, FECHA, HORAACCION, ELEMENTOMODIFICADO } = registro;
   try {
-    const [result] = await pool.query('INSERT INTO REGISTROACCION (IDACCION, IDUSUARIO, IP, FECHA, HORAACCION, ELEMENTOMODIFICADO) VALUES (?, ?, ?, ?, ?, ?)', 
-      [IDACCION, IDUSUARIO, IP, FECHA, HORAACCION, ELEMENTOMODIFICADO]);
+    const { IDACCION, IDUSUARIO, IP, FECHA, HORAACCION, ELEMENTOMODIFICADO } = registro;
+    const [result] = await pool.query(
+      'INSERT INTO REGISTROACCION (IDACCION, IDUSUARIO, IP, FECHA, HORAACCION, ELEMENTOMODIFICADO) VALUES (?, ?, ?, ?, ?, ?)',
+      [IDACCION, IDUSUARIO, IP, FECHA, HORAACCION, ELEMENTOMODIFICADO]
+    );
     return result.insertId;
   } catch (error) {
     throw new Error(error.message);
