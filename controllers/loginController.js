718514dc4f69251966ec68a4ea080e1836d07ad4
cryobus <jaav2801@gmail.com>
2024-05-29 03:35:18 -0400

    Falla login con bitacora

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -14,36 +14,7 @@ const loginUser = async (req, res) => {
     if (!isMatch) {
       return res.status(401).json({ message: 'Contraseña incorrecta' });
     }
-        // Obtener IP del cliente
-        const ipAPI = "https://api.ipify.org?format=json";
-        const response = await fetch(ipAPI);
-        const data = await response.json();
-        const ipAddress = data.ip;
-    
-        // Obtener fecha y hora actuales
-        const now = new Date();
-        const fecha = now.toISOString().split('T')[0];
-        const hora = now.toTimeString().split(' ')[0];
-    
-        // Registrar la acción en la bitácora
-        await addRegistro({
-          IDACCION: 1, // ID de INICIAR SESION
-          IDUSUARIO: user.ID,
-          IP: ipAddress,
-          FECHA: fecha,
-          HORAACCION: hora,
-          ELEMENTOMODIFICADO: 'LOGIN'
-        });
-    
-        // Emitir evento de nueva acción
-        io.emit('nuevaAccion', {
-          IDACCION: 1,
-          IDUSUARIO: user.ID,
-          IP: ipAddress,
-          FECHA: fecha,
-          HORAACCION: hora,
-          ELEMENTOMODIFICADO: 'LOGIN'
-        });
+       
     res.json({ message: 'Login exitoso', user: { id: user.ID, apellidos: user.APELLIDOS, nombres: user.NOMBRES }, role: user.IDROL });
   } catch (error) {
     res.status(500).json({ message: 'Error al iniciar sesión', error });
