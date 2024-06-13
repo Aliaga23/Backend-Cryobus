c24fdc128729b1a3a4036e1cb091002f0f3bf1b2
cryobus <jaav2801@gmail.com>
2024-06-13 03:36:47 -0400

    M

--- routes/rolConductorRoutes.js
+++ routes/rolConductorRoutes.js
@@ -1,13 +1,14 @@
 // routes/rolConductorRoutes.js
 const express = require('express');
 const { getRolConductors, getRolConductorById, createRolConductor, updateRolConductor, deleteRolConductor } = require('../controllers/rolConductorController');
+const { authenticate, authorize } = require('../middleware/auth');
 
 const router = express.Router();
 
-router.get('/', getRolConductors);
-router.get('/:id', getRolConductorById);
-router.post('/', createRolConductor);
-router.put('/:id', updateRolConductor);
-router.delete('/:id', deleteRolConductor);
+router.get('/', authenticate, authorize([27]), getRolConductors);
+router.get('/:id', authenticate, authorize([27]),  getRolConductorById);
+router.post('/', authenticate, authorize([10]),  createRolConductor);
+router.put('/:id', authenticate, authorize([10]), updateRolConductor);
+router.delete('/:id', authenticate, authorize([10]),  deleteRolConductor);
 
 module.exports = router;
