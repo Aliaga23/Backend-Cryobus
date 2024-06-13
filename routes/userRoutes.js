8d811f7abaaf10086596d30ae7c9a40b0e563ebf
cryobus <jaav2801@gmail.com>
2024-06-13 03:40:47 -0400

    m

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,8 +4,8 @@ const { authenticate, authorize } = require('../middleware/auth');
 
 const router = express.Router();
 
-router.get('/', authenticate, authorize([32]), getUsers); // Ver usuarios
-router.get('/:id', authenticate, authorize([32]), getUserById); // Ver usuarios
+router.get('/', authenticate, authorize([30]), getUsers); // Ver usuarios
+router.get('/:id', authenticate, authorize([30]), getUserById); // Ver usuarios
 router.post('/', authenticate, authorize([13]), createUser); // Gestionar usuarios
 router.put('/:id', authenticate, authorize([13]), updateUser); // Gestionar usuarios
 router.delete('/:id', authenticate, authorize([13]), deleteUser); // Gestionar usuarios
