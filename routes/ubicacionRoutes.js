fe6b0813b87453fb8285c9f88d8815573eb86615
cryobus <jaav2801@gmail.com>
2024-06-13 08:52:24 -0400

    asidjh

--- routes/ubicacionRoutes.js
+++ routes/ubicacionRoutes.js
@@ -5,10 +5,10 @@ const { authenticate, authorize } = require('../middleware/auth');
 
 const router = express.Router();
 
-router.get('/', authenticate, authorize([3]), getUbicaciones); // Ver ubicaciones
-router.get('/:id', authenticate, authorize([3]), getUbicacionById); // Ver ubicación específica
-router.post('/', authenticate, authorize([17]), createUbicacion); // Crear ubicación
-router.put('/:id', authenticate, authorize([17]), updateUbicacion); // Actualizar ubicación
-router.delete('/:id', authenticate, authorize([17]), deleteUbicacion); // Eliminar ubicación
+router.get('/', authenticate, authorize([17]), getUbicaciones); // Ver ubicaciones
+router.get('/:id', authenticate, authorize([17]), getUbicacionById); // Ver ubicación específica
+router.post('/', authenticate, authorize([3]), createUbicacion); // Crear ubicación
+router.put('/:id', authenticate, authorize([3]), updateUbicacion); // Actualizar ubicación
+router.delete('/:id', authenticate, authorize([3]), deleteUbicacion); // Eliminar ubicación
 
 module.exports = router;
