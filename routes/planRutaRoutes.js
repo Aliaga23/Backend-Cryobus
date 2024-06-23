e7dc8d56a45ff8648f72f616498b917960d9819d
cryobus <jaav2801@gmail.com>
2024-06-23 14:46:46 -0400

    plan

--- routes/planRutaRoutes.js
+++ routes/planRutaRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { getPlanRutas, getPlanRutaById, createPlanRuta, updatePlanRuta, deletePlanRuta } = require('../controllers/planRutaController');
+const { authenticate, authorize } = require('../middleware/auth');
 
 const router = express.Router();
 
-router.get('/', getPlanRutas);
-router.get('/:id', getPlanRutaById);
-router.post('/', createPlanRuta);
-router.put('/:id', updatePlanRuta);
-router.delete('/:id', deletePlanRuta);
+router.get('/',  authenticate, authorize([8]),getPlanRutas);
+router.get('/:id',  authenticate, authorize([8]),getPlanRutaById);
+router.post('/', authenticate, authorize([25]) ,createPlanRuta);
+router.put('/:id',  authenticate, authorize([25]),updatePlanRuta);
+router.delete('/:id',  authenticate, authorize([25]),deletePlanRuta);
 
 module.exports = router;
