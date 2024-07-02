8cfa031331c6fc10582ea90a39505733f5f3f0b6
cryobus <jaav2801@gmail.com>
2024-07-02 03:57:56 -0400

    nota decre

--- models/notaTrasladoModel.js
+++ models/notaTrasladoModel.js
@@ -7,14 +7,14 @@ const getNotasTraslado = async () => {
 
 const createNotaTraslado = async (notaTraslado) => {
   const { fechaLlegada, horaLlegada, fechaSalida, horaSalida, nroCamion, idPlanRuta } = notaTraslado;
-  const [result] = await pool.query('INSERT INTO NOTATRASLADO (FECHALLEGADA, HORALLEGADA, FECHASALIDA, HORASALIDA, NROCAMION, IDPLANRUTA) VALUES (?, ?, ?, ?, ?, ?)', [fechaLlegada, horaLlegada, fechaSalida, horaSalida, nroCamion, idPlanRuta]);
-  return result.insertId;
+  const result = await pool.query('INSERT INTO NOTATRASLADO (FECHALLEGADA, HORALLEGADA, FECHASALIDA, HORASALIDA, NROCAMION, IDPLANRUTA) VALUES (?, ?, ?, ?, ?, ?)', [fechaLlegada, horaLlegada, fechaSalida, horaSalida, nroCamion, idPlanRuta]);
+  return result[0];
 };
 
 const updateNotaTraslado = async (nro, notaTraslado) => {
   const { fechaLlegada, horaLlegada, fechaSalida, horaSalida, nroCamion, idPlanRuta } = notaTraslado;
-  const [result] = await pool.query('UPDATE NOTATRASLADO SET FECHALLEGADA = ?, HORALLEGADA = ?, FECHASALIDA = ?, HORASALIDA = ?, NROCAMION = ?, IDPLANRUTA = ? WHERE NRO = ?', [fechaLlegada, horaLlegada, fechaSalida, horaSalida, nroCamion, idPlanRuta, nro]);
-  return result;
+  const result = await pool.query('UPDATE NOTATRASLADO SET FECHALLEGADA = ?, HORALLEGADA = ?, FECHASALIDA = ?, HORASALIDA = ?, NROCAMION = ?, IDPLANRUTA = ? WHERE NRO = ?', [fechaLlegada, horaLlegada, fechaSalida, horaSalida, nroCamion, idPlanRuta, nro]);
+  return result[0];
 };
 
 const deleteNotaTraslado = async (nro) => {
@@ -25,8 +25,11 @@ const deleteNotaTraslado = async (nro) => {
     // Eliminar la nota de traslado
     await conn.query('DELETE FROM NOTATRASLADO WHERE NRO = ?', [nro]);
     
-    // Decrementar los valores de NRO mayores al eliminado
-    await conn.query('UPDATE NOTATRASLADO SET NRO = NRO - 1 WHERE NRO > ?', [nro]);
+    // Decrementar los valores de NRO mayores al eliminado y actualizar las tablas dependientes
+    await conn.query('UPDATE NOTAENTREGA SET NRONOTATRASLADO = NRONOTATRASLADO - 1 WHERE NRONOTATRASLADO > ?', [nro]);
+    await conn.query('UPDATE DETALLECONDUCTOR SET NRONOTATRASLADO = NRONOTATRASLADO - 1 WHERE NRONOTATRASLADO > ?', [nro]);
+    await conn.query('SET @count = 0;');
+    await conn.query('UPDATE NOTATRASLADO SET NRO = @count := @count + 1 ORDER BY NRO;');
     
     await conn.commit();
   } catch (error) {
